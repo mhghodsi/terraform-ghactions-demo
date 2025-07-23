8ae6edfe027994eff57db1ee27b33731225f5e7c
Hossein Ghodsi <mhghodsi@gmail.com>
2025-07-22 19:41:17 -0700

    Update Terraform workflow and configuration
    - Add conditional execution for Terraform Apply step in CI workflow
    - Change AWS region from us-west-2 to us-east-1 in main.tf
    - Add .gitignore file to exclude Terraform-related files
    - Initialize .terraform.lock.hcl with provider details

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+# Configure the AWS provider to use the us-east-1 region
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
+# Create an S3 bucket with the specified name
 resource "aws_s3_bucket" "mybucket" {
   bucket = "gh-actions-demo-bucket-hossein"
+}
+
+# Set the access control list (ACL) for the S3 bucket to private
+resource "aws_s3_bucket_acl" "mybucket_acl" {
+  bucket = aws_s3_bucket.mybucket.id
   acl    = "private"
 }
