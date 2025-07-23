5e59f44308dec898eb0229c9c71cd3999d1bfd6d
Hossein Ghodsi <mhghodsi@gmail.com>
2025-07-22 19:59:23 -0700

    Update S3 bucket name to include a random suffix for uniqueness

--- main.tf
+++ main.tf
@@ -3,9 +3,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Generate a 4-character random suffix for uniqueness
+resource "random_string" "suffix" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
 # Create an S3 bucket with the specified name
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "gh-actions-demo-bucket-hossein"
+  bucket = "gh-actions-demo-bucket-${random_string.suffix.result}"
 }
 
 # Set the access control list (ACL) for the S3 bucket to private
