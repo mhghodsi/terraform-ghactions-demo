35078ec0a361c6e2629ead279c1234027563770b
Hossein Ghodsi <mhghodsi@gmail.com>
2025-08-19 15:15:01 -0500

    Refactor backend configuration: switch from local to S3 backend for state management, enhancing security and scalability.

--- backend.tf
+++ backend.tf
@@ -1,5 +1,11 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket               = "hgh-terraform-state"
+    region               = "us-east-1"
+    key                  = "infra.tfstate"          # constant file name
+    workspace_key_prefix = "terraform/state/demo"   # folder/prefix you choose
+    dynamodb_table       = "terraform-locks"        # recommended for state locking
+    encrypt              = true
+    # kms_key_id         = "alias/terraform"        # optional: SSE-KMS
   }
 }
